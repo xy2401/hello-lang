718f3d2c560ffaf19dc24370e35c3a4baa0a84f0
xy2401 <xy2401@163.com>
2026-08-30 22:58:49 +0800

    style: apply wasm browser workbench layout and styles to lang playground

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -7,6 +7,7 @@ import VersionDiff from './components/VersionDiff.vue';
 import DockerTooling from './components/DockerTooling.vue';
 import LanguageContainerWorkbench from './components/LanguageContainerWorkbench.vue';
 import './doc-baseline.css';
+import './browser-workbench.css';
 import './custom.css';
 
 export default {
